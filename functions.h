a2b81edd1735d6d8942172adf6e6ab54ace4965d
Annant05 <annantguptaneema@gmail.com>
2018-06-28 00:21:58 +0530

    stack implementation

--- functions.h
+++ functions.h
@@ -461,5 +461,69 @@ void fibonacci_rec() {
     }
 }
 
+// stack Start
+int MAXSIZE;
+int tos = -1;
+
+void displayStack(int stack[]) {
+    for (int i = 0; i <= tos; i++) {
+        printf("%d, ", stack[i]);
+    }
+}
+
+void push(int stack[]) {
+    if (tos == MAXSIZE - 1) {
+        printf("Cannot insert -> Stack is full\n");
+    } else {
+        tos++;
+        printf("Insert Element  : ");
+        scanf("%d", &stack[tos]);
+    }
+}
+
+
+void pop(int stack[]) {
+    if (tos == -1) {
+        printf("Stack is empty\n");
+
+    } else {
+        printf("Element  %d : deleted\n", stack[tos]);
+        tos--;
+    }
+}
+
+void stack_reset(int stack[]) {
+    for (int i = 0; i < MAXSIZE; i++) {
+        stack[i] = 0;
+    }
+}
+
+void stack_using_array() {
+    //Stack using array
+    printf("\nEnter the size of array : ");
+    scanf("%d", &MAXSIZE);// &MAXSIZE = 10);
+    int stack[MAXSIZE];
+    stack_reset(stack);
+    int ch = 1;
+    while (ch) {
+        displayStack(stack);
+        printf("\n1: insert \t 2: delete \t 0: Exit   : ");
+        scanf("%d", &ch);
+        switch (ch) {
+            case 1:
+                push(stack);
+                break;
+            case 2:
+                pop(stack);
+                break;
+            case 0:
+                ch = 0;
+                break;
+            default:
+                continue;
+        }
+    }
+}
+//Stack stop
 
 #pragma clang diagnostic pop
